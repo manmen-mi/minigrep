b9792f97d315dc8bf408b327acfa938512566f77
manmen-mi <manmen.mi1375@gmail.com>
2024-12-07 20:31:43 +0900

    env: `IGNORE_CASE`

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,4 @@
-use std::{error::Error, fs};
-
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut result = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            result.push(line);
-        }
-    }
-
-    result
-}
+use std::{env, error::Error, fs};
 
 #[cfg(test)]
 mod tests {
@@ -25,11 +14,52 @@ Pick three.";
         
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        )
+    }
 }
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+
+    result
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut result = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            result.push(line);
+        }
+    }
+
+    result
+}
+
+
 
 pub struct Config {
     pub query: String,
     pub path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -41,13 +71,22 @@ impl Config {
         let query = args[1].clone();
         let path = args[2].clone();
     
-        Ok(Config { query, path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config { query, path, ignore_case })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let raw = fs::read_to_string(config.path)?;
-    for line in search(&config.query, &raw) {
+
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &raw)
+    } else {
+        search(&config.query, &raw)
+    };
+
+    for line in results {
         println!("{line}");
     }
 
