937dc264d8bad1384814f63a183376f4d2af9d33
manmen-mi <manmen.mi1375@gmail.com>
2024-12-07 20:10:30 +0900

    refactor: Config

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,30 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let path = &args[2];
+    let config = Config::new(&args);
 
-    println!("Searching {}", query);
-    println!("In file {}", path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.path);
 
-    let raw = fs::read_to_string(path)
+    let raw = fs::read_to_string(config.path)
         .expect("failed: can not read file");
+}
 
-    println!("With text: \n{raw}");
+
+struct Config {
+    query: String,
+    path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("not enough args");
+        }
+
+        let query = args[1].clone();
+        let path = args[2].clone();
+    
+        Config { query, path }
+    }
 }
