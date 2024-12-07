6e61085471732174e155c1f174fb8dc5fd8e70b2
manmen-mi <manmen.mi1375@gmail.com>
2024-12-07 19:47:12 +0900

    first print

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let path = &args[2];
+
+    println!("Searching {}", query);
+    println!("In file {}", path);
+
+    let raw = fs::read_to_string(path)
+        .expect("failed: can not read file");
+
+    println!("With text: \n{raw}");
 }
